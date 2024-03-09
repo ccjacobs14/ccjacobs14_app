3224f5fdad33e962a5deadc253603840c7fa75c1
ccjacobs14 <ccjacobs14@yahoo.com>
2024-03-08 19:40:49 -0500

    moving app to remote

---
+++ src/main/java/com/ibt/ccjacobs14_app/App.java
@@ -0,0 +1,13 @@
+package com.ibt.ccjacobs14_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is just a test." );
+    }
+}
